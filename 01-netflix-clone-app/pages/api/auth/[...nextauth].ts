ff7ff9f688d7736b86ec62448b0e40cdd0ad8bac
Luigui A. Parodi <98921293+LPDev93@users.noreply.github.com>
2023-11-27 20:32:56 -0500

    Update [...nextauth].ts

--- 01-netflix-clone-app/pages/api/auth/[...nextauth].ts
+++ 01-netflix-clone-app/pages/api/auth/[...nextauth].ts
@@ -6,7 +6,7 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { compare } from "bcrypt";
 import prismadb from "@/libs/prismadb";
 
-export const authOptions: AuthOptions = {
+const authOptions: AuthOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID || "",
